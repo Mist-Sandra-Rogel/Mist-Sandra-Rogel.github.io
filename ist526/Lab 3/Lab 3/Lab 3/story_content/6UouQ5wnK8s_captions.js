ec4267d1acbe5997609dc30f30a93b96615fda37
Mist-Sandra-Rogel <myfirmsr831@gmail.com>
2024-02-20 22:20:16 -0800

    edit

--- ist526/Lab 3/Lab 3/Lab 3/story_content/6UouQ5wnK8s_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.84.31722.0%0D%0A%0D%0A00:00:00.187%20--%3E%2000:00:02.926%0D%0AThe%20personalization%20principle%20...states%20that,%0D%0A%0D%0A"}')
