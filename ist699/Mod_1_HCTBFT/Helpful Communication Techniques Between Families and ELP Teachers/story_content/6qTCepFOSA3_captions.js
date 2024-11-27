6e39408471d0100fc75176c4e19547c4b6f6e037
Mist-Sandra-Rogel <myfirmsr831@gmail.com>
2024-11-27 14:05:58 -0800

    testing

--- ist699/Mod_1_HCTBFT/Helpful Communication Techniques Between Families and ELP Teachers/story_content/6qTCepFOSA3_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.93.33359.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:03.733%0D%0ACongratulations!%20You%20have%20successfully%20concluded%20this%20module.%0D%0A%0D%0A"}')
