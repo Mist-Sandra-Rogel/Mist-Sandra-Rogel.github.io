58d9a62692aa627134340642e684fac534c98f8f
Mist-Sandra-Rogel <myfirmsr831@gmail.com>
2024-12-02 01:30:40 -0800

    Big

---
+++ ist699/Mod_3_HCTBFT/Pre assessment/story_content/5wXKPUehToB_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.93.33359.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:03.083%0D%0AHow%20can%20poor%20communication%20contribute%20to%20teacher%20burnout?%0D%0A%0D%0A"}')
